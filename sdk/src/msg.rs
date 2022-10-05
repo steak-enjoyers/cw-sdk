00224a1b1d4c77b5acaf439a1071dc371b85de0e
larry <26318510+larry0x@users.noreply.github.com>
2022-10-05 16:30:32 +0100

    tx type definition

--- sdk/src/msg.rs
+++ sdk/src/msg.rs
@@ -2,6 +2,30 @@ use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Binary, Coin};
 
 #[cw_serde]
+pub struct Tx {
+    /// Transaction body
+    pub body: TxBody,
+    /// The sender's secp256k1 public key. Optional if the accounts already exists in the state.
+    pub pubkey: Option<Binary>,
+    /// Secp256k1 signature; the content is `sha256(JSON.stringify(txbody))`, signed by the private
+    /// key corresponding to `pubkey`.
+    pub signature: Binary,
+}
+
+/// Body of the transaction. This is what the sender needs to sign.
+#[cw_serde]
+pub struct TxBody {
+    /// The sender's address
+    pub sender: String,
+    /// Wasm messages to be executed
+    pub msgs: Vec<SdkMsg>,
+    /// Identifier of the chain where this tx is to be broadcasted. Used to prevent reply attacks.
+    pub chain_id: String,
+    /// The sender's sequence number. Used to prvent replay attacks.
+    pub sequence: u64,
+}
+
+#[cw_serde]
 pub enum SdkMsg {
     StoreCode {
         wasm_byte_code: Binary,
