db48419404436b1502003c6401a1363db8226c35
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2022-10-14 08:39:43 +0200

    Update sdk/src/hash.rs
    Co-authored-by: Larry Engineer <26318510+larry0x@users.noreply.github.com>

--- sdk/src/hash.rs
+++ sdk/src/hash.rs
@@ -1,8 +1,7 @@
 use sha2::{Digest, Sha256};
 
 /// Perform a SHA-256 hash of the given bytes
-pub fn sha256(bytes: impl AsRef<[u8]>) -> Vec<u8> 
-{
+pub fn sha256(bytes: impl AsRef<[u8]>) -> Vec<u8> {
     let mut hasher = Sha256::new();
     hasher.update(bytes);
     hasher.finalize().to_vec()
