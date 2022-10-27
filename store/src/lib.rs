ec6c57298888712b2064fcc9adefbecb7ffc60b9
larry <26318510+larry0x@users.noreply.github.com>
2022-10-27 05:53:18 +0100

    remove mock from default feature

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -2,12 +2,12 @@ mod cache;
 mod prefix;
 mod store;
 
-#[cfg(feature = "mock")]
-mod mock;
-
 pub use cache::CacheStore;
 pub use prefix::PrefixStore;
 pub use store::Store;
+
+#[cfg(feature = "mock")]
+mod mock;
 
 #[cfg(feature = "mock")]
 pub use mock::MockStore;
