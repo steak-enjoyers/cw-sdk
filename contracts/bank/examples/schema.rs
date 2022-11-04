ad9c472ffd86dc0f02b8363ef699809839545307
larry <26318510+larry0x@users.noreply.github.com>
2022-11-04 22:21:19 +0000

    update bank contract schema

--- contracts/bank/examples/schema.rs
+++ contracts/bank/examples/schema.rs
@@ -1,10 +1,11 @@
 use cosmwasm_schema::write_api;
 
-use cw_bank::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_bank::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, SudoMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
+        sudo: SudoMsg,
         execute: ExecuteMsg,
         query: QueryMsg,
     }
